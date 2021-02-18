ebdc7536b7852683a0bcc50997e155c17552d930
Suskiiii <susannah.jarventausta@gmail.com>
2021-02-18 21:07:10 +0200

    Parse data to JS object and send to children

--- src/components/AnalyzeData.js
+++ src/components/AnalyzeData.js
@@ -3,6 +3,8 @@ import '../styles.css';
 import OpeningPrice from './OpeningPrice';
 import UpwardTrend from './UpwardTrend';
 import VolumeAndPriceChange from './VolumeAndPriceChange';
+import * as Papa from 'papaparse';
+import * as moment from 'moment';
 
 export default function AnalyzeData(props) {
   const [stockFile, setStockFile] = useState('');
@@ -20,26 +22,78 @@ export default function AnalyzeData(props) {
 
   function updateStockDataTimeRange(dataInRange) {
     setStockDataTimeRange(dataInRange);
-    //This one should be sent as props to children
   }
 
-  function parseFile() {
-    //Parse data from stockFile to right format
-    //Put result to stockData and stockDataTimeRange
+  function stringToDate(string) {
+    let date = moment(string, 'MM/DD/YYYY').toDate();
+    return date;
+  }
+
+  function removeDollar(string) {
+    let amount = string.substring(1);
+    amount = parseFloat(amount);
+    return amount;
+  }
+
+  function parseJSON(jsonObject) {
+    let tempArray = [];
+    let index = 0;
+
+    for (let element of jsonObject) {
+      if (element.Date !== '') {
+        let date = stringToDate(element.Date);
+        let volume = parseInt(element.Volume, 10);
+        let closeLast = removeDollar(element['Close/Last']);
+        let open = removeDollar(element.Open);
+        let high = removeDollar(element.High);
+        let low = removeDollar(element.Low);
+
+        tempArray.push({
+          id: index,
+          Date: date,
+          CloseLast: closeLast,
+          Volume: volume,
+          Open: open,
+          High: high,
+          Low: low,
+        });
+        index = index + 1;
+      }
+    }
+    updateStockData(tempArray);
+    updateStockDataTimeRange(tempArray);
+  }
+
+  function parseFile(e) {
+    Papa.parse(e.target.files[0], {
+      header: true,
+      delimiter: ', ',
+      complete: function (results) {
+        parseJSON(results.data);
+      },
+    });
   }
 
   function onStockFileChange(e) {
-    updateStockFile(e.target.value);
+    updateStockFile(e.target.files[0]);
+    parseFile(e);
   }
 
-  function onAnalyzeFileClick() {
-    parseFile();
+  if (stockDataTimeRange == [] || stockDataTimeRange == '') {
+    return (
+      <div className="AnalyzeData">
+        <input type="file" accept=".csv" onChange={onStockFileChange} />
+        <div className="UpwardTrend"></div>
+      </div>
+    );
   }
 
   return (
     <div className="AnalyzeData">
-      <input type="file" value={stockFile} onChange={onStockFileChange} />
-      <button onClick={onAnalyzeFileClick}>Analyze</button>
+      <input type="file" accept=".csv" onChange={onStockFileChange} />
+      <div>
+        StockData date from parent: {stockDataTimeRange[0].Date.toString()}
+      </div>
       <UpwardTrend stockData={stockDataTimeRange}></UpwardTrend>
       <VolumeAndPriceChange
         stockData={stockDataTimeRange}
--- src/components/UpwardTrend.js
+++ src/components/UpwardTrend.js
@@ -2,5 +2,9 @@ import React, {useState, useEffect} from 'react';
 import '../styles.css';
 
 export default function UpwardTrend(props) {
-  return <div className="UpwardTrend"></div>;
+  return (
+    <div className="UpwardTrend">
+      StockData date from child: {props.stockData[0].Date.toString()}
+    </div>
+  );
 }
