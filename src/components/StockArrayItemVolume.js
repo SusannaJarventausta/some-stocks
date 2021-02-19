931f11ae1a4d581b7c069199e44c0911a50ff57b
Suskiiii <susannah.jarventausta@gmail.com>
2021-02-19 19:50:42 +0200

    Sort data by volume and price changes

---
+++ src/components/StockArrayItemVolume.js
@@ -0,0 +1,16 @@
+import React, {useState, useEffect} from 'react';
+import '../styles.css';
+
+export default function StockArrayItemVolume(props) {
+  function priceChange(high, low) {
+    let difference = high - low;
+    return parseFloat(difference.toFixed(6));
+  }
+  return (
+    <div className="StockArrayItemVolume">
+      <p>{props.Date.toString()}</p>
+      <p>{props.Volume}</p>
+      <p>{priceChange(props.High, props.Low)}</p>
+    </div>
+  );
+}
--- src/components/VolumeAndPriceChange.js
+++ src/components/VolumeAndPriceChange.js
@@ -1,6 +1,59 @@
 import React, {useState, useEffect} from 'react';
 import '../styles.css';
+import StockArrayItemVolume from './StockArrayItemVolume';
 
 export default function VolumeAndPriceChange(props) {
-  return <div className="VolumeAndPriceChange"></div>;
+  const [stockArray, setStockArray] = useState([]);
+
+  function updateStockArray(array) {
+    setStockArray(array);
+  }
+
+  useEffect(() => {
+    updateStockArray(sortStocks());
+  }, [props.stockData]);
+
+  function compare(a, b) {
+    if (a.Volume > b.Volume) {
+      return -1;
+    }
+
+    if (a.Volume < b.Volume) {
+      return 1;
+    }
+    if (a.High - a.Low < b.High - b.Low) {
+      return 1;
+    }
+    if (a.High - a.Low > b.High - b.Low) {
+      return -1;
+    }
+    //Equal in volume and price change
+    return 0;
+  }
+
+  function sortStocks() {
+    let stockArrayTemp = [...props.stockData];
+    stockArrayTemp.sort(compare);
+    return stockArrayTemp;
+  }
+
+  return (
+    <div className="VolumeAndPriceChange">
+      <h2>List of Volume and price changes</h2>
+      <div className="stockVolumeList">
+        {stockArray.map((item) => (
+          <StockArrayItemVolume
+            key={item.Id}
+            Id={item.Id}
+            Date={item.Date}
+            CloseLast={item.CloseLast}
+            Volume={item.Volume}
+            Open={item.Open}
+            High={item.High}
+            Low={item.Low}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
